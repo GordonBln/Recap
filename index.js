6f4149d0e9a1ceb190752da52ce620c3c462d554
GordonBln <75781762+GordonBln@users.noreply.github.com>
2021-04-28 11:39:34 +0200

    typing text

--- index.js
+++ index.js
@@ -6,12 +6,32 @@ console.log("hello World");
 // 3.2 change the colour of the "div"
 let button = document.getElementById("myButton")
 console.log(button);
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
     console.log("i am clicked")
+    console.log(event)
     let square = document.querySelector(".square");
     console.log(square)
     square.style.backgroundColor = "red";
 })
-//document.getElementById("square").addEventListener("click", function(){
-    //document.getElementById("demo").innerHTML = "Hello World";
- // }); 
+
+
+//giving a field where to type the text
+//identify  this field
+//while is typing (user typing event)
+// get the text of the field
+// identify where the text should appear
+// put the text in the square
+
+let textfield = document.querySelector("#textfield");
+console.log(textfield);
+const myCallbackfuntion = (event) => {
+    console.log("i am typing")
+
+
+    let text = event.target.value
+    console.log(text)
+    let square = document.querySelector(".square");
+    console.log(square)
+    square.innerHTML = text;
+}
+textfield.addEventListener("input", myCallbackfuntion)
